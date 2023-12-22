1cb3f232ef52c15e691a571b65629c8f4bb9e63a
Neil Sainsbury <neil.sainsbury@foxtel.com.au>
2023-12-22 13:06:50 +1000

    Day 7, Part 1

---
+++ src/main/kotlin/Day07.kt
@@ -0,0 +1,88 @@
+import Hand.Type.*
+import java.io.File
+
+data class Hand(
+    val cards: String,
+    val bid: Int
+) : Comparable<Hand> {
+    private val type = evaluateHand()
+
+    private fun evaluateHand(): Type {
+        val counts = cards.groupingBy { it }.eachCount()
+
+        return when (counts.size) {
+            1 -> FIVE_OF_A_KIND
+            2 -> if (counts.any { it.value == 4 }) FOUR_OF_A_KIND else FULL_HOUSE
+            3 -> if (counts.any { it.value == 3 }) THREE_OF_A_KIND else TWO_PAIR
+            4 -> ONE_PAIR
+            5 -> HIGH_CARD
+            else -> throw IllegalStateException()
+        }
+    }
+
+    // zero if equal, positive if > argument, negative if < argument
+    override fun compareTo(other: Hand): Int {
+        // Same hand
+        if (cards == other.cards)
+            return 0
+
+        if (type == other.type) {
+            // Same type - evaluate on a card-by-card basis
+            for (i in 0 until 5) {
+                val c = CardLabel.fromChar(cards[i])
+                val otherC = CardLabel.fromChar(other.cards[i])
+
+                if (c != otherC) {
+                    return c.ordinal - otherC.ordinal
+                }
+            }
+
+            // Impossible to get to here
+            throw IllegalStateException()
+        } else {
+            return type.ordinal - other.type.ordinal
+        }
+    }
+
+    enum class Type {
+        HIGH_CARD,
+        ONE_PAIR,
+        TWO_PAIR,
+        THREE_OF_A_KIND,
+        FULL_HOUSE,
+        FOUR_OF_A_KIND,
+        FIVE_OF_A_KIND
+    }
+
+    enum class CardLabel {
+        N2, N3, N4, N5, N6, N7, N8, N9, T, J, Q, K, A;
+
+        companion object {
+            fun fromChar(c: Char): CardLabel =
+                enumValueOf<CardLabel>(if (c.isDigit()) "N$c" else "$c")
+        }
+    }
+}
+
+
+fun main() {
+    val hands = readInput()
+    val ranked = hands.sorted()
+
+    val result = ranked.withIndex().sumOf { (i, hand) ->
+        (i + 1L) * hand.bid
+    }
+
+    println(result)
+}
+
+fun readInput(): List<Hand> {
+    return File("data/day07_input.txt")
+        .readLines()
+        .map { line ->
+            val (cards, bid) = line.split(' ')
+            Hand(cards, bid.toInt())
+        }
+}
+
+
